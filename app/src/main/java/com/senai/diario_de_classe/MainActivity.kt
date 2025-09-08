aa97d1d76f31e3b34abb50680aa335af3febfd00
Matheus de Oliveira <matheusndoliveira@gmail.com>
2025-09-08 16:05:58 -0300

    funcionando

--- app/src/main/java/com/senai/diario_de_classe/MainActivity.kt
+++ app/src/main/java/com/senai/diario_de_classe/MainActivity.kt
@@ -7,27 +7,37 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.senai.diario_de_classe.data.Aluno
+import com.senai.diario_de_classe.data.DataSource
 import com.senai.diario_de_classe.ui.theme.DiarioDeClasseTheme
 
 class MainActivity : ComponentActivity() {
@@ -55,14 +65,15 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun CardAluno(
     modifier: Modifier = Modifier,
-    nomeAluno: String = stringResource(R.string.nome_padrao),
-    turmaAluno: String = stringResource(R.string.turma_padrao),
-    foto: Int = R.drawable.foto_perfil
+//    nomeAluno: String = stringResource(R.string.nome_padrao),
+//    turmaAluno: String = stringResource(R.string.turma_padrao),
+//    foto: Int = R.drawable.foto_perfil
+    aluno: Aluno = Aluno("Matheus", curso = "Desenvolvimento de Sistemas")
 ) {
     Card(
         modifier = modifier
             .fillMaxWidth()
-            .padding(25.dp),
+            .padding(start = 15.dp, end=15.dp),
     ) {
         Row(
             modifier
@@ -70,8 +81,8 @@ fun CardAluno(
                 .padding(start = 15.dp, end = 15.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
-            FotoPerfilAluno(Modifier.size(100.dp), idImagem = foto)
-            InformacoesDoAluno(fontSizeNome = 20.sp, nomeAluno = nomeAluno, turmaAluno = turmaAluno)
+            FotoPerfilAluno(Modifier.size(100.dp), idImagem = aluno.foto)
+            InformacoesDoAluno(fontSizeNome = 20.sp, aluno = aluno)
         }
     }
 }
@@ -79,7 +90,20 @@ fun CardAluno(
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun DiarioDeClasseApp(modifier: Modifier = Modifier) {
-    CardAluno(modifier)
+    val layoutDirection = LocalLayoutDirection.current
+    Surface(
+        modifier
+            .fillMaxSize()
+            .statusBarsPadding()
+            .padding(
+                start = WindowInsets.safeDrawing.asPaddingValues()
+                    .calculateStartPadding(layoutDirection),
+                end = WindowInsets.safeDrawing.asPaddingValues()
+                    .calculateEndPadding(layoutDirection),
+            ),
+    ) {
+        ListaDeAlunos(modifier, DataSource().carregarAlunos())
+    }
 }
 
 
@@ -95,8 +119,9 @@ fun FotoPerfilAluno(modifier: Modifier = Modifier, idImagem: Int) {
 @Composable
 fun InformacoesDoAluno(
     modifier: Modifier = Modifier,
-    nomeAluno: String,
-    turmaAluno: String,
+//    nomeAluno: String,
+//    turmaAluno: String,
+    aluno: Aluno,
     fontSizeNome: TextUnit = 15.sp,
     fontSizeTurma: TextUnit = 15.sp,
     fontSizeGeral: TextUnit? = null
@@ -104,8 +129,8 @@ fun InformacoesDoAluno(
     val fontSizeN = fontSizeGeral ?: fontSizeNome
     val fontSizeT = fontSizeGeral ?: fontSizeTurma
     Column(modifier.fillMaxWidth(), horizontalAlignment = Alignment.Start) {
-        Text(nomeAluno, fontSize = fontSizeN)
-        Text(turmaAluno, fontSize = fontSizeT)
+        Text(aluno.nome, fontSize = fontSizeN)
+        Text(aluno.curso, fontSize = fontSizeT)
     }
 }
 
@@ -113,7 +138,7 @@ fun InformacoesDoAluno(
 fun ListaDeAlunos(modifier: Modifier = Modifier, listaDeAlunos: List<Aluno>) {
     LazyColumn {
         items(listaDeAlunos) { aluno ->
-            CardAluno(modifier, aluno.nome, aluno.curso, aluno.foto)
+            CardAluno(modifier, aluno)
         }
     }
 }
--- app/src/main/java/com/senai/diario_de_classe/data/Aluno.kt
+++ app/src/main/java/com/senai/diario_de_classe/data/Aluno.kt
@@ -1,7 +1,11 @@
 package com.senai.diario_de_classe.data
 
+import androidx.annotation.DrawableRes
+import androidx.compose.ui.res.stringResource
+import com.senai.diario_de_classe.R
+
 data class Aluno(
-    val nome: String,
-    val foto: Int,
-    val curso: String
+    val nome: String = "Nome do Aluno",
+    @DrawableRes val foto: Int = R.drawable.foto_perfil,
+    val curso: String = "Curso do Aluno"
 )
